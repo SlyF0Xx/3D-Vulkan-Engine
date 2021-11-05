b76f60fbb4d2069422178c8fcb1f8a6ce2fb7b35
SlyF0Xx <max.tolstukhin@mail.ru>
2021-11-05 20:26:52 +0300

    Fix texture mapping on primitives (finally)

--- Game/CubeEntity.cpp
+++ Game/CubeEntity.cpp
@@ -17,10 +17,10 @@ namespace diffusion {
           PrimitiveColoredVertex{-0.5f, -0.5f,  0.5f, {1.0f, 1.0f}},
           PrimitiveColoredVertex{-0.5f, -0.5f, -0.5f, {1.0f, 0.0f}},
 
-          PrimitiveColoredVertex{ 0.5f,  0.5f,  0.5f, {0.0f, 1.0f}},
-          PrimitiveColoredVertex{ 0.5f,  0.5f, -0.5f, {0.0f, 0.0f}},
-          PrimitiveColoredVertex{-0.5f,  0.5f,  0.5f, {1.0f, 1.0f}},
-          PrimitiveColoredVertex{-0.5f,  0.5f, -0.5f, {1.0f, 0.0f}}
+          PrimitiveColoredVertex{ 0.5f,  0.5f,  0.5f, {1.0f, 1.0f}},
+          PrimitiveColoredVertex{ 0.5f,  0.5f, -0.5f, {1.0f, 0.0f}},
+          PrimitiveColoredVertex{-0.5f,  0.5f,  0.5f, {0.0f, 1.0f}},
+          PrimitiveColoredVertex{-0.5f,  0.5f, -0.5f, {0.0f, 0.0f}}
         },
         {
             // forward
